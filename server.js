7e1ff6cd715ac58f3f57905d0e8d09a6f6ea3169
Keith Barker <kbarker@ipdanalytics.com>
2018-11-20 18:15:36 -0500

    Add initial project list to home page and fix CORS issue with api.

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require('cors');
 const app = express();
 const PORT = 90;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("client/build"));
