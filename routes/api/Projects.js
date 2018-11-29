b1822190cf7587f8fa08516df92a7ff22cfc7290
yeikel <email@yeikel.com>
2018-11-28 23:41:34 -0500

    Update request type for project creation from POST to PUT for consistency

--- routes/api/Projects.js
+++ routes/api/Projects.js
@@ -3,7 +3,7 @@ const ProjectsController = require("../../controllers/ProjectsController");
 
 router.route("/")
   .get(ProjectsController.list)
-  .post(ProjectsController.create)
+  .put(ProjectsController.create)
   .delete(ProjectsController.delete);
 
 
