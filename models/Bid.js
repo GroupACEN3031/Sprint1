daf17065505594f81f21df0cbc3a3430992bc87b
yeikel <email@yeikel.com>
2018-12-09 22:50:49 -0500

    Add 'order' property to bids in order to rank them

--- models/Bid.js
+++ models/Bid.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const Bid = new Schema({
     id: { type: String, required: false },
     projectID: { type: String, required: true },
-    teamID: { type: String, required: true }
+    teamID: { type: String, required: true },
+    order: { type: Number, required: true }
 });
 
 module.exports = mongoose.model("Bid", Bid);
