f81a1c0f83fa38c8db90d5c47517fbbe761d139a
michaelhowerter <michaelhowerter@ufl.edu>
2018-12-11 12:14:00 -0800

    edited Profile.js to allow nicer UI
    Columns separate the label from the input.

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,57 +1,62 @@
 import React from "react";
 import {Container } from "../../components/Grid";
-import {Col,Row} from "react-bootstrap";
+import Jumbotron from "../../components/Jumbotron";
+import {Col,Row,Grid,Navbar,Nav,NavItem,NavDropdown,MenuItem} from "react-bootstrap";
 
 const Profile = () =>
   <Container fluid>
-    <Row>
+      <h1>WebApps @ UF Student Team Portal</h1>
+      <h3>Profile Page</h3>
+      <h3></h3>    
+      <Row>
       <Col size="md-10">
-        <h1>WebApps @ UF Student Team Portal</h1>
-        <h3>Profile Page</h3>
+
+        <form>
+          <label for="firstName" class="col-sm-3 col-form-label">
+            First Name:
+          </label>
+            <input type="text" name="name" />
+        </form>
+        <form>
+          <label for="lastName" class="col-sm-3 col-form-label">
+            Last Name:
+          </label>
+            <input type="text" last="lastname" />
+        </form>
+        <form>
+          <label for="email" class="col-sm-3 col-form-label">
+            Email:
+         </label>
+            <input type="text" email="email" />
+        </form>
+        <form>
+          <label for="password" class="col-sm-3 col-form-label">
+            Password:
+          </label>
+            <input type="text" password="password" />
+        </form>
+        <form>
+          <label for="password2" class="col-sm-3 col-form-label">
+            Re-enter Password:
+          </label>
+            <input type="text" last="password2" />
+        </form>
+        <form>
+          <label for="photo" class="col-sm-3 col-form-label">
+            Upload Photo:
+          </label>
+            <input type="text" last="photo" />
+        </form>
+        <form>
+          <label for="skills" class="col-sm-3 col-form-label">
+            Skills:
+          </label>
+            <input type="text" last="skills" />
+        </form>
         <form>
           <label>
-            First Name:
-            <input type="text" name="name" />
+            <input type="submit" value="Submit" />
           </label>
-        </form>
-        <form>
-        <label>
-            Last Name:
-            <input type="text" last="lastname" />
-          </label>
-        </form>
-        <form>
-        <label>
-            Email:
-            <input type="text" email="email" />
-          </label>
-        </form>
-        <form>
-        <label>
-            Password:
-            <input type="text" password="password" />
-          </label>
-        </form>
-        <form>
-        <label>
-            Re-enter Password:
-            <input type="text" last="password2" />
-          </label>
-        </form>
-        <form>
-        <label>
-            Upload Photo:
-            <input type="text" last="photo" />
-          </label>
-        </form>
-        <form>
-        <label>
-            Skills:
-            <input type="text" last="skills" />
-          </label>
-        </form>
-        <form>
-        <input type="submit" value="Submit" />
         </form>
       </Col>
     </Row>
